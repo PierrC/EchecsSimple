243fc634a738d8a29a59c3824bde1af16aca2349
PierrC <PierrC@users.noreply.github.com>
2017-07-15 12:29:47 -0400

    added new Project ChessAppGDI: - chess Board built with gdi and resizes with window - added annotations with a boolean that will be used to turn them on and off - added piece images to resources - added image of white pawn to board, but it does not resize with window

---
+++ PC/Echecs/csharp/FirstChessApp/ChessAppGDI/ChessApp.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ChessAppGDI
+{
+    partial class ChessApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.boardPanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(553, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startGameToolStripMenuItem
+            // 
+            this.startGameToolStripMenuItem.Name = "startGameToolStripMenuItem";
+            this.startGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.startGameToolStripMenuItem.Text = "Start Game";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.75768F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.24232F));
+            this.tableLayoutPanel1.Controls.Add(this.boardPanel, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(553, 438);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // boardPanel
+            // 
+            this.boardPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.boardPanel.Location = new System.Drawing.Point(3, 3);
+            this.boardPanel.Name = "boardPanel";
+            this.boardPanel.Size = new System.Drawing.Size(423, 432);
+            this.boardPanel.TabIndex = 0;
+            this.boardPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.boardPanel_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ChessApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 462);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ChessApp";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startGameToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel boardPanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PC/Echecs/csharp/FirstChessApp/ChessAppGDI/ChessApp.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessAppGDI
+{
+    public partial class ChessApp : Form
+    {
+        /// All variable
+        /// 
+        static int boardPanel_x, boardPanel_y;
+        static int square_x, square_y, square;
+        Pen mainPen = new Pen(Color.Black, 1);
+        Graphics g = null;
+        Font myFont = new Font("Times New Roman", 16);
+
+        Boolean showBoardAnnotation = true;
+
+        
+        public ChessApp()
+        {
+            InitializeComponent();
+            this.MinimumSize = new System.Drawing.Size(700, 600);
+        }
+
+        
+        private void boardPanel_Paint(object sender, PaintEventArgs e)
+        {
+            boardPanel_x = boardPanel.Width;
+            boardPanel_y = boardPanel.Height;
+            g = boardPanel.CreateGraphics();
+            g.Clear(Color.White);
+            // The Board must be the first draw method called
+            drawBoard();
+
+            drawPieces();
+        }
+
+        
+        private void drawPieces()
+        {
+        //    Image.FromFile("..\\..\\Resources\\Icons\\key-icon.png");
+
+            Image image = Image.FromFile("C:\\Users\\pierr\\Documents\\GitHub\\EchecsSimple\\PC\\Echecs\\csharp\\FirstChessApp\\ChessAppGDI\\Resources\\whitePawn.gif");
+            Image image2 = new Bitmap(Properties.Resources.whitePawn);
+            Rectangle srcRect = new Rectangle(30, 80, 60, 60);
+            g.DrawImage(image, new Point(100, 100));
+            g.DrawImage(image2, srcRect);
+           // g.DrawImage(image, 100, 100, srcRect, units);
+        }
+
+        
+
+
+
+        private void drawBoard()
+        {
+            square_x = boardPanel_x / 9;
+            square_y = boardPanel_y / 9;
+            square = Math.Min(square_x, square_y);
+            for (int i = 0; i < 9; i++)
+            {
+                // Vertical lines
+                Point p1 = new Point(square / 2, square / 2 + square * i);
+                Point p2 = new Point(square / 2 + square * 8, square / 2 + square * i);
+                g.DrawLine(mainPen, p1, p2);
+
+                // Horizontal lines
+                Point p3 = new Point(square / 2 + square * i, square / 2);
+                Point p4 = new Point(square / 2 + square * i, square / 2 + square * 8);
+                g.DrawLine(mainPen, p3, p4);
+                
+            }
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    int p1 = square / 2 + (2 * i * square);
+                    int p2 = square / 2 + (2 * j * square);
+                    g.FillRectangle(Brushes.Black, p1 , p2, square, square);
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int p1 = square / 2 + square + (2 * i * square);
+                    int p2 = square / 2 + square + (2 * j * square);
+                    g.FillRectangle(Brushes.Black, p1, p2, square, square);
+                }
+            }
+
+            if (showBoardAnnotation)
+            {
+                for(int i = 1; i < 9; i++)
+                {
+                    g.DrawString((9 - i).ToString(), myFont, Brushes.Black, square / 16, square * i - (square / 4));
+                    g.DrawString( Char.ConvertFromUtf32(65 + i - 1) , myFont, Brushes.Black, square * i - (square / 4), square / 16);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+
+
+}
